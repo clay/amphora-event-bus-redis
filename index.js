616602121f0c5ca81a627abc76c01a49a4021153
Jon Winton <winton.jon@gmail.com>
2018-10-01 12:52:12 -0400

    cleanup

--- index.js
+++ index.js
@@ -3,20 +3,33 @@
 const Redis = require('ioredis'),
   redis = new Redis(process.env.CLAY_BUS_HOST),
   os = require('os'),
-  HOSTNAME = os.hostname();
+  HOSTNAME = os.hostname(),
+  PID = process.pid;
 var log = require('./log').setup({ file: __filename });
 
+/**
+ * Connect to Redis
+ *
+ * @returns {Object}
+ */
 function connect() {
   log('info', `Connecting to Redis Event Bus at ${process.env.CLAY_BUS_HOST}`);
   module.exports.client = new Redis(process.env.CLAY_BUS_HOST);
+
   return { publish };
 }
 
+/**
+ * Write to the bus
+ *
+ * @param {String} topic
+ * @param {Object|Array} msg
+ */
 function publish(topic, msg) {
   if (module.exports.client) {
     redis.publish(topic, JSON.stringify({
       hostname: HOSTNAME,
-      pid: process.pid,
+      pid: PID,
       msg
     }));
   }
